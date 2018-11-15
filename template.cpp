c1b3658decfd3b1932e67b3fc0baf9b90b2dbb09
drhasler <45033724+drhasler@users.noreply.github.com>
2018-11-15 15:42:15 +0100

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define pb push_back
+#define mp make_pair
+#define IOS ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+
+int main() {
+    IOS;
+}
